a8f85b722af9b7c33f99d1d3347d8bec5bcc0e7c
eftrout <dfrost783@gmail.com>
2025-08-08 17:46:30 -0500

    Signed Full name

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "site_enabled" {
 variable "bucket_name" {
   description = "Base name for S3 bucket"
   type        = string
-  default     = "hello-worldT"
+  default     = "hello-worldt"
 }
 
 # Common tags for resources
